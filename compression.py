7c7d9f77150eaec2e146f48debc7639012309c6b
musembi <musembinzioki@gmail.com>
2017-11-22 00:24:37 +0300

    Basic implementation of all 4 flows

--- compression.py
+++ compression.py
@@ -14,47 +14,67 @@ class Compression(QMainWindow):
     
     def __init__(self):
         super(Compression, self).__init__()
-        self.setGeometry(0, 0, 650, 550)
+        self.setGeometry(0, 0, 450, 350)
         self.centerOnScreen()
-        self.prompt_target_file()
+        self.show()
+        btn_compress = QPushButton("Compress", self)
+        btn_compress.move(100, 100)
+        btn_compress.clicked.connect(self.pick_compress_target_file)
+        btn_compress.show()
+
+        btn_decompress = QPushButton("Decompress", self)
+        btn_decompress.move(250, 100)
+        btn_decompress.clicked.connect(self.pick_decompress_target_file)
+        btn_decompress.show()
+
+        btn_encrypt = QPushButton("Encrypt", self)
+        btn_encrypt.move(100, 200)
+        btn_encrypt.clicked.connect(self.pick_encrypt_target_file)
+        btn_encrypt.show()
+
+        btn_decrypt = QPushButton("Decrypt", self)
+        btn_decrypt.move(250, 200)
+        btn_decrypt.clicked.connect(self.pick_decrypt_target_file)
+        btn_decrypt.show()
+        # self.prompt_target_file()
 
 
     def centerOnScreen (self):
         resolution = QDesktopWidget().screenGeometry()
         self.move((resolution.width() / 2) - (self.frameSize().width() / 2),
                   (resolution.height() / 2) - (self.frameSize().height() / 2)) 
-        
-        
-    def prompt_target_file(self):
-        self.setWindowTitle('Eka Compress')
 
-        btn = QPushButton("Choose file to compress", self)
-        btn.clicked.connect(self.pick_target_file)
-        btn.move(50, 50)
-        btn.show()
+    def pick_compress_target_file(self):
+        self.compress_target_file = QFileDialog.getOpenFileName(self, "Pick a file to compress")
+        self.pick_compress_destination()
 
-        self.show()
+    def pick_compress_destination(self):
+        self.compress_destination = QFileDialog. getExistingDirectory (self, "Pick a destination for your compressed file")
+        self.compress_file(self.compress_target_file, self.compress_destination)
 
-    def prompt_destination(self):
+    def pick_decompress_target_file(self):
+        self.decompress_target_file = QFileDialog.getOpenFileName(self, "Pick a file to decompress")
+        self.pick_decompress_destination()
 
-        self.setWindowTitle('Eka Compress')
+    def pick_decompress_destination(self):
+        self.decompress_destination = QFileDialog. getExistingDirectory (self, "Pick a destination for your decompressed file")
+        self.decompress_file(self.decompress_target_file, self.decompress_destination)
 
-        btn = QPushButton("Choose the destination folder", self)
-        btn.clicked.connect(self.pick_destination)
-        btn.move(50, 50)
-        btn.show()
+    def pick_encrypt_target_file(self):
+        self.encrypt_target_file = QFileDialog.getOpenFileName(self, "Pick a file to encrypt")
+        self.pick_encrypt_destination()
 
-        self.show()
+    def pick_encrypt_destination(self):
+        self.encrypt_destination = QFileDialog. getExistingDirectory (self, "Pick a destination for your encrypted file")
+        self.encrypt_file(self.encrypt_target_file, self.encrypt_destination)
 
-    def pick_target_file(self):
-        self.target_file = QFileDialog.getOpenFileName()
-        self.prompt_destination()
+    def pick_decrypt_target_file(self):
+        self.decrypt_target_file = QFileDialog.getOpenFileName(self, "Pick a file to decrypt")
+        self.pick_decrypt_destination()
 
-    def pick_destination(self):
-        self.destination = QFileDialog.getOpenFileName()
-        self.compress_file(self.target_file, self.destination)
-
-    # could have a transitional screen here showing target and destination
+    def pick_decrypt_destination(self):
+        self.decrypt_destination = QFileDialog. getExistingDirectory (self, "Pick a destination for your decrypted file")
+        self.decrypt_file(self.decrypt_target_file, self.decrypt_destination)
 
     def progress_bar(self):
         self.completed = 0
@@ -72,19 +92,73 @@ class Compression(QMainWindow):
             target = dominus.readbytes(target_file)
             compressed = dominus.compress(target)
             dominus.writebytes(destination, compressed)
-            self.alert_success()
+            self.alert_compress_success()
+        except Exception:
+            raise
+
+    def decompress_file(self, target_file, destination):
+        # self.progress_bar()
+        try:
+            target = dominus.readbytes(target_file)
+            compressed = dominus.compress(target)
+            dominus.writebytes(destination, compressed)
+
+            infile = dominus.readbytes(target_file)
+            uncompressed = dominus.decompress(infile)
+            outfile = open(destination + '/out.uncompressed','w')
+            for bt in uncompressed:
+                outfile.write(bt)
+            self.alert_decompress_success()
+        except Exception:
+            raise
+
+    def encrypt_file(self, target_file, destination):
+        # self.progress_bar()
+        try:
+            target = dominus.readbytes(target_file)
+            compressed = dominus.compress(target)
+            dominus.writebytes(destination, compressed)
+            self.alert_encrypt_success()
+        except Exception:
+            raise
+
+    def decrypt_file(self, target_file, destination):
+        # self.progress_bar()
+        try:
+            target = dominus.readbytes(target_file)
+            compressed = dominus.compress(target)
+            dominus.writebytes(destination, compressed)
+
+            infile = dominus.readbytes(target_file)
+            uncompressed = dominus.decompress(infile)
+            outfile = open(destination + '/out.uncompressed','w')
+            for bt in uncompressed:
+                outfile.write(bt)
+            self.alert_decrypt_success()
         except Exception:
             raise
 
     def alert_progress(self):
         QMessageBox.about(self, "Eka Compress", "Please wait while your file compresses")
 
-    def alert_success(self):
+    def alert_compress_success(self):
         QMessageBox.about(self, "Eka Compress", "Successfully compressed {} \n Check {} for the compressed file".format(
-            self.target_file.split('/')[-1], self.destination))
+            self.compress_target_file.split('/')[-1], self.compress_destination))
+
+    def alert_decompress_success(self):
+        QMessageBox.about(self, "Eka Compress", "Successfully decompressed {} \n Check {} for the decompressed file".format(
+            self.decompress_target_file.split('/')[-1], self.decompress_destination))
+
+    def alert_encrypt_success(self):
+        QMessageBox.about(self, "Eka Compress", "Successfully encrypted {} \n Check {} for the encrypted file".format(
+            self.encrypt_target_file.split('/')[-1], self.encrypt_destination))
+
+    def alert_decrypt_success(self):
+        QMessageBox.about(self, "Eka Compress", "Successfully decrypted {} \n Check {} for the decrypted file".format(
+            self.decrypt_target_file.split('/')[-1], self.decompress_destination))
 
     def alert_failure(self):
-        QMessageBox.about(self, "Eka Compress", "An error ocurred while compressing. Please try again")
+        QMessageBox.about(self, "Eka Compress", "An error ocurred while processing. Please try again")
 
 
 def main():    
